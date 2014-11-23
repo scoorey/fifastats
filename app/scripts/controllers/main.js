acc587c3522c36a8fa7f84330601c38b219f981a
scoorey <scoorey@gmail.com>
2014-11-23 13:56:25 +1000

    build2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,9 +17,14 @@ angular.module('fifastatsApp')
       'fred'
     ];
 
+    $scope.formationStyle = {
+   width:'300px'
+};
+
     $scope.recentGames = {};
-
-
+	$scope.currentGame = [];
+	$scope.gameSelected = null;
+	$scope.afterGameSelected = 'blah';
     $scope.results = {};
     $scope.results.result = 'result';
 
@@ -28,25 +33,49 @@ angular.module('fifastatsApp')
  	$scope.formations = {};
     $scope.formations.formation  ='formation';
     $scope.formations.theirformation = 'theirformation';
-     $scope.formations.options = [
+    $scope.formations.options = [
                '3-5-2',
                '4-1-2-1-2',
             '4-3-3', 
              '5-4-1'
              ];
 
-     $scope.divs = {};
-     $scope.divs.div = 'division';
+    $scope.divs = {};
+    $scope.divs.div = 'division';
     $scope.divs.options = ['10','9','8','7','6','5','4','3','2','1'];
 
+
+    $scope.shotsOnTarget = null;
+    $scope.opponentShotsOnTarget = null;
+    $scope.goalsFor = null;
+    $scope.goalsAgainst = null;
+    $scope.possessionFor = null;
+    $scope.possessionAgainst = null;
+    $scope.passAccuracyFor = null;
+    $scope.passAccuracyAgainst = null;
+    $scope.tacklesFor = null;
+    $scope.tacklesAgainst = null;
+    $scope.foulsFor = null;
+    $scope.foulsAgainst = null;
+    $scope.yellowsFor = null;
+    $scope.yellowsAgainst = null;
+    $scope.redsFor = null;
+    $scope.redsAgainst = null;
+    $scope.offsidesFor = null;
+    $scope.offsidesAgainst = null;
+    $scope.teamUsed = null;
+    $scope.teamAgainst = null;
+
+    $scope.myUserName = null;
+    $scope.opponentUserName = null;
 		
     $scope.addTodo = function () {
 
 	   $scope.todos.push($scope.formations.formation);
 	   $scope.todos.push($scope.divs.div);
-	   $scope.todos.push($scope.goalsfor);
+	   $scope.todos.push($scope.goalsFor);
 	  $scope.todo = '';
-	  $scope.goalsfor = '';
+	  $scope.goalsFor = '';
 	  $scope.divs.div = '';
 	  $scope.formations.formation = '';
 	};
@@ -85,6 +114,50 @@ angular.module('fifastatsApp')
 		//console.log(src);
 		return src;
 	};
+
+  $scope.selectRestaurant = function(row) {
+  
+  	/*jshint camelcase: false */
+  	console.log($scope.recentGames[row].self.user_info);
+   	for (var i=0; i< $scope.recentGames.length; i++)
+    {
+        if (row === i)
+        {
+        	console.log($scope.recentGames[i]);
+        	$scope.currentGame.push($scope.recentGames[i]);
+        }
+    }
+    $scope.recentGames = $scope.currentGame;
+  
+
+    $scope.gameSelected = 'ell';
+  
+  	$scope.shotsOnTarget = $scope.recentGames[0].self.stats.shot_on_target;
+    $scope.opponentShotsOnTarget = $scope.recentGames[0].opponent.stats.shot_on_target;
+    $scope.goalsFor = $scope.recentGames[0].self.stats.score;
+    $scope.goalsAgainst = $scope.recentGames[0].opponent.stats.score;
+    $scope.possessionFor = $scope.recentGames[0].self.stats.possession;
+    $scope.possessionAgainst = $scope.recentGames[0].opponent.stats.possession;
+    $scope.passAccuracyFor = $scope.recentGames[0].self.stats.pass_made / $scope.recentGames[0].self.stats.pass_attempts * 100;
+    $scope.passAccuracyAgainst = $scope.recentGames[0].opponent.stats.pass_made / $scope.recentGames[0].opponent.stats.pass_attempts * 100;
+    $scope.tacklesFor = $scope.recentGames[0].self.stats.tackles;
+    $scope.tacklesAgainst = $scope.recentGames[0].opponent.stats.tackles;
+    $scope.foulsFor = $scope.recentGames[0].self.stats.fouls;
+    $scope.foulsAgainst = $scope.recentGames[0].opponent.stats.fouls;
+    $scope.yellowsFor = $scope.recentGames[0].self.stats.yellow_cards;
+    $scope.yellowsAgainst = $scope.recentGames[0].opponent.stats.yellow_cards;
+    $scope.redsFor = $scope.recentGames[0].self.stats.red_cards;
+    $scope.redsAgainst = $scope.recentGames[0].opponent.stats.red_cards;
+    $scope.offsidesFor = $scope.recentGames[0].self.stats.offsides;
+    $scope.offsidesAgainst = $scope.recentGames[0].opponent.stats.offsides;
+    $scope.teamUsed = $scope.recentGames[0].self.stats.team;
+    $scope.teamAgainst = $scope.recentGames[0].opponent.stats.team;
+
+    $scope.myUserName = $scope.recentGames[0].self.user_info;
+    $scope.opponentUserName = $scope.recentGames[0].opponent.user_info;
+  //angular.element($0).scope() 
+	};
+ 
 
 
   });
